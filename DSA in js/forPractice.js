e5d02aa4b2332fe0b6140fe4f24c55510fa695a3
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-09-28 10:49:52 +0530

    leetcode prob 2 solved

--- DSA in js/forPractice.js
+++ DSA in js/forPractice.js
@@ -11,3 +11,46 @@
 //   }
 // }
 // findStudent(studentsDatabase, "erick");
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+let a = new Node(3);
+let b = new Node(4);
+let c = new Node(5);
+a.next = b;
+b.next = c;
+let d = new Node(6);
+let e = new Node(7);
+let f = new Node(8);
+d.next = e;
+e.next = f;
+
+console.log(a);
+console.log(d);
+console.log(Math.floor(13 / 10));
+
+function addTwoNumbers(l1, l2) {
+  let carry = 0;
+  let dummy = new Node(0);
+  let current = dummy;
+  while (l1 !== null && l2 !== null) {
+    let x = l1 !== null ? l1.val : 0;
+    let y = l2 !== null ? l2.val : 0;
+    let sum = carry + x + y;
+    carry = Math.floor(sum / 10);
+    current.next = new Node(sum % 10);
+    current = current.next;
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+  if (carry > 0) {
+    current.next = new Node(carry);
+  }
+  return JSON.stringify(dummy.next);
+}
+console.log(addTwoNumbers(a, d));
---
+++ Leetcode Problems in js/2.addTwoNumbers.js
@@ -0,0 +1,34 @@
+// 2. Add Two Numbers
+// You are given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Example 1:
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+// Example 2:
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+
+// Example 3:
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+function addTwoNumbers(l1, l2) {
+  let dummy = new ListNode(0),
+    carry = 0,
+    current = dummy;
+
+  while (l1 || l2 || carry) {
+    let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    l1 = l1?.next;
+    l2 = l2?.next;
+  }
+  return dummy.next;
+}
