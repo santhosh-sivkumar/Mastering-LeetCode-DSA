0b03feae2b8bdb63f8a7578a3774f4f2d8609a0a
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-12-05 12:53:35 +0530

    added prob 11 solution

---
+++ 11. Container With Most Water.js
@@ -0,0 +1,19 @@
+// 11. Container With Most Water
+
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    maxArea = Math.max(maxArea, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+};
